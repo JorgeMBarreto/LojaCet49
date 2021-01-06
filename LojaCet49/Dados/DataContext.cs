36ef06ca1b5552abda9e9a28824223eb11200003
Jorge Barreto <Jorge.Estenaga.Barreto@formandos.cinel.pt>
2021-01-06 10:20:46 +0000

    Criar a base de dados e tabela produtos

---
+++ LojaCet49/Dados/DataContext.cs
@@ -0,0 +1,24 @@
+﻿using LojaCet49.Dados.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LojaCet49.Dados
+{
+    public class DataContext : DbContext
+    {
+
+        public DbSet<Produto> Produtos { get; set; }
+
+
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+    }
+
+}
